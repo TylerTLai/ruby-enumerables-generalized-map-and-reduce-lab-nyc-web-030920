e09b89c6600d1ad4c5c9ee25c30cfc501a5dbd69
Tyler Lai <lai.t.tyler@gmail.com>
2020-02-15 20:52:40 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,17 @@
-# Your Code Here
+def map(arr)
+  
+  arr2 = []
+  i = 0 
+  
+  while i < arr.length do
+    
+    arr2 << arr[i] * -1
+    arr2 << yield
+    
+  end 
+
+  arr2
+  
+end 
+
+map(n) {|n| n * -1}
